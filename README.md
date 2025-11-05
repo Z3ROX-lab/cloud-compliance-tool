b90571b02f787d0e45a01b835d4766da91269f7d
zerotrustXXX <stefilou1971@gmail.com>
2025-11-05 09:35:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Cloud Compliance Tool
+
+Une solution open source pour auditer la conformitÃ© des architectures cloud.
+
+## Installation
+1. Installez Docker et Node.js.
+2. Lancez docker-compose up pour dÃ©marrer les services.
---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Cloud Compliance Tool</h1>
+      <p>Bienvenue ! Utilisez cette interface pour auditer vos environnements cloud.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
